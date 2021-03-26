10e77af3190bec751088ed624735a986947e1c45
ConnorCourtney <connorcourtney123@gmail.com>
2021-03-26 16:08:07 -0400

    index of all head and details

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ fetch('http://myapi-profstream.herokuapp.com/api/8debf4/books')
     .then(data => 
         {const container = document.querySelector('#viewArea');
         removeAllChildNodes(container);
+        var head = document.createElement('p');
+        var t = document.createTextNode("Index of All Books");
+        head.appendChild(t);
+        document.getElementById('viewArea').appendChild(head);
         {for(i = 0; i < data.length; i++){allBook(data[i])}
+
+        
    
 }});
 
@@ -39,6 +45,10 @@ function printBook(x) {
     const container = document.querySelector('#viewArea');
     removeAllChildNodes(container);
 
+    var head = document.createElement('p');
+    var t = document.createTextNode("Details for " + x.title);
+    head.appendChild(t);
+
     var bookTitle = document.createElement('p');
     var textnode = document.createTextNode(x.title) 
     bookTitle.appendChild(textnode);
@@ -55,7 +65,7 @@ function printBook(x) {
     bookImage.src = x.image
     
     
-
+    document.getElementById('viewArea').appendChild(head);
     document.getElementById('viewArea').appendChild(bookTitle);
     document.getElementById('viewArea').appendChild(bookAuthor);
     document.getElementById('viewArea').appendChild(bookRelease);
@@ -78,30 +88,9 @@ function removeAllChildNodes(parent){
 
  function clickBook(bookId){
         document.getElementById(bookId).addEventListener("click", function(){
-            // console.log(bookId);
-            fetch('http://myapi-profstream.herokuapp.com/api/8debf4/books/' + bookId) 
+                fetch('http://myapi-profstream.herokuapp.com/api/8debf4/books/' + bookId) 
                 .then(response => response.json())
                 .then(data => printBook(data))
         })
-        // document.getElementById("2938").addEventListener("click", function(){
-        //     console.log('test click');
-        // })
-        // document.getElementById("2939").addEventListener("click", function(){
-        //     console.log('test click');
-        // })
-        // document.getElementById("2940").addEventListener("click", function(){
-        //     console.log('test click');
-        // })
-        // document.getElementById("2941").addEventListener("click", function(){
-        //     console.log('test click');
-        // })
-        // document.getElementById("2942").addEventListener("click", function(){
-        //     console.log('test click');
-        // })
-        // document.getElementById("2943").addEventListener("click", function(){
-        //     console.log('test click');
-        // })
-        // document.getElementById("2944").addEventListener("click", function(){
-        //     console.log('test click');
-    //     })
+        
      }}
